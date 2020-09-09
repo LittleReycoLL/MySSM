41ff77c4e101d50ae359e786ba3d35b769999779
LittleReycoLL <q1037978533@vip.qq.com>
2020-09-09 19:24:18 +0800

    delete

--- EasyBuy/src/main/java/com/ssm/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.ssm.dao;
-
-import com.ssm.model.User;
-
-public interface UserDao {
-
- User findByUsername(String username);
-}
--- EasyBuy/src/main/java/com/ssm/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ssm.service;
-
-import com.ssm.model.User;
-
-public interface UserService {
-    User checkLogin(String username, String password);
-}
